982ab650866a8a941eb5a997e38507a2c0e0c3c3
budja8 <dc79826@gmail.com>
2021-12-07 11:13:50 -0600

    Add Help Screen

--- HelpScreen.java
+++ HelpScreen.java
@@ -4,6 +4,13 @@ public class HelpScreen extends World
 {
     public HelpScreen()
     {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(900, 600, 1); 
+    }
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("b")){
+            Greenfoot.setWorld(new MainScreen());
+        }
     }
 }
